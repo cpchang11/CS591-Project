2a6a22af2d42a7b56b06d6d19e1f7d1e3ab6f427
Kevin Sujanto <kevinsuj@bu.edu>
2020-12-10 00:56:46 -0500

    change variables name to make more sense

--- Python/multi.py
+++ Python/multi.py
@@ -2,8 +2,8 @@ import pandas as pd
 import statsmodels.api as sm
 
 df = pd.read_csv('../CSV/state_cases_political_score.csv')
-X = df[['age', 'avg2', 'highschool', 'college', 'political_score']]
-y = df['c/p']
+X = df[['age', 'avg_income', 'highschool', 'college', 'political_score']]
+y = df['covid_per_population']
 
 ## fit a OLS model with intercept on TV and Radio
 X = sm.add_constant(X)
