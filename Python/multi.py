f4ff0005914debbd38c75fab384878b6478fbcef
Kevin Sujanto <kevinsuj@bu.edu>
2020-12-10 00:50:13 -0500

    add multi.py

---
+++ Python/multi.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import statsmodels.api as sm
+
+df = pd.read_csv('../CSV/state_cases_political_score.csv')
+X = df[['age', 'avg', 'highschool', 'college', 'd or r', 'political_score']]
+y = df['c/p']
+
+## fit a OLS model with intercept on TV and Radio
+X = sm.add_constant(X)
+
+est = sm.OLS(y, X).fit()
+print(est.summary())
